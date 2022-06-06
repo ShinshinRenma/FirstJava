7d88af0590f6b786543694119d62da0afddb1871
ShinshinRenma <ryan.andrew.layman@gmail.com>
2022-06-06 09:17:09 -1000

    Initial commit

---
+++ FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Ryan Layman.");
+        System.out.println("I am 37 years old.");
+        System.out.println("My hometown is Honolulu, HI.");
+    }
+}
